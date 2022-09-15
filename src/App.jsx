d58a4d98331ac12bc6b00291917fac6ec09b00b9
argjend551 <argjend551@hotmail.com>
2022-09-15 20:34:12 +0200

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Header } from "./Header";
-import "../scss/main.scss";
+
 export default function App() {
   return (
     <Router>
-      <header></header>
       <main>
         <Routes>
           <Route path="/" element={<StartPage />} />
