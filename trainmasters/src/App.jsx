6b3473265f20086931e26f33e09cf7379dd38496
Nikolai Kocev <nikolai_kocev@hotmail.com>
2022-09-15 19:57:07 +0200

    cleaned upp App.jsx

--- trainmasters/src/App.jsx
+++ trainmasters/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './scss/main.scss';
+
 export default function App() {
   return (
     <Router>
@@ -9,7 +9,6 @@ export default function App() {
       <main>
         <Routes>
           <Route path='/' element={<StartPage />} />
-          <Route path='/about-us' element={<AboutUsPage />} />
         </Routes>
       </main>
       <footer>
