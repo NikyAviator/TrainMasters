412235e29fe75944ac7eaa0c8ec524ed34bfe3f1
Nikolai Kocev <nikolai_kocev@hotmail.com>
2022-09-15 15:04:09 +0200

    Added basic backend from template

---
+++ backend/Server.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const RestApi = require('./RestApi');
+
+module.exports = class Server {
+  app = express();
+  port = 4000;
+
+  constructor() {
+    this.start();
+  }
+
+  start() {
+    this.app.use(express.json());
+    new RestApi(this.app);
+    this.serveDist();
+    let message = `Backend listening on port ${this.port}`;
+    this.app.listen(this.port, () => console.log(message));
+  }
+
+  serveDist() {
+    // serve built dist (production React created with npm run build)
+    this.app.use(express.static(path.join(__dirname, '../', 'dist')));
+    // make hard reload of frontend routes work
+    // serve index.html if there is no matching backend route
+    this.app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../', 'dist', 'index.html'));
+    });
+  }
+};
---
+++ backend/index.js
@@ -0,0 +1,2 @@
+const Server = require('./Server');
+new Server();
