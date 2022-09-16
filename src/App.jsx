68a67d13402436c6b4f2bae6743488255b027af1
Seans99 <sean99@live.nl>
2022-09-16 17:16:11 +0200

    Added pages and components folders. Also created routes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import StartPage from "./components/pages/StartPage";
+import BookingPage from "./components/pages/BookingPage";
+import TicketsPage from "./components/pages/TicketsPage";
+import LogInPage from "./components/pages/LogInPage";
+import RegisterPage from "./components/pages/RegisterPage";
 
 export default function App() {
   return (
@@ -6,7 +11,10 @@ export default function App() {
       <main>
         <Routes>
           <Route path="/" element={<StartPage />} />
-          <Route path="/about-us" element={<AboutUsPage />} />
+          <Route path="/booking" element={<BookingPage />} />
+          <Route path="/tickets" element={<TicketsPage />} />
+          <Route path="/login" element={<LogInPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Routes>
       </main>
       <footer>
---
+++ src/components/pages/LogInPage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function LogInPage() {
+  return <div>StartPage</div>;
+}
