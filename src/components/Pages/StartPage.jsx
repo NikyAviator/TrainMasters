11be0bfafceeda06f66f857ede893bde9abb158d
Sean Schelvis <sean.schelvis@hotmail.com>
2022-09-26 13:11:58 +0200

    changed text

--- src/components/Pages/StartPage.jsx
+++ src/components/Pages/StartPage.jsx
@@ -7,7 +7,7 @@ export default function StartPage() {
   return (
     <div className="hero-container">
       <h1>Tågmästarna</h1>
-      <p>Vart vill du åka?</p>
+      <p>Vart vill du resa?</p>
       <div className="hero-btn">
         <Button
           className="btn"
