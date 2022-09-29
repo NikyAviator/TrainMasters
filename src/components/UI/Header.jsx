ce82ef68e52f653bd9c3dd761769ef22812bbda5
Sean Schelvis <sean.schelvis@hotmail.com>
2022-09-29 10:03:40 +0200

    Fixed header AGAIN

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from './Button';
 
-import '../../../scss/main.scss';
+import '../../../scss/Header.scss';
 
 function Header() {
   const [click, setClick] = useState(false);
