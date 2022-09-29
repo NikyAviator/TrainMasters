1cbbaa5cd06be5cdef96bf5fd84599ef1afaba53
Nikolai Kocev <nikolai_kocev@hotmail.com>
2022-09-29 19:26:03 +0200

    Datepicker modified, also secret modified!

--- src/components/Ticket/TicketDatePicker.jsx
+++ src/components/Ticket/TicketDatePicker.jsx
@@ -1,28 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Form from 'react-bootstrap/Form';
 
-const TicketDatePicker = (props) => {
-  const [enteredDate, setEnteredDate] = useState('');
+const TicketDatePicker = ({ setWeekend }) => {
+  function checkweekday(e) {
+    let day = new Date(e);
+    let getdate = day.getUTCDay();
+    if (getdate === 0 || getdate === 6) setWeekend(true);
+    else {
+      setWeekend(false);
+    }
+  }
 
-  const dateChangeHandler = (event) => {
-    setEnteredDate(event.target.value);
-  };
   return (
     <>
       <Form>
         <Form.Group>
           <Form.Control
             type='date'
-            name='start'
+            name='duedate'
             placeholder='Choose date of your departure'
             min='2022-01-01'
-            value={enteredDate}
-            onChange={dateChangeHandler}
+            onChange={(e) => checkweekday(e.target.value)}
           />
         </Form.Group>
       </Form>
     </>
   );
 };
-
 export default TicketDatePicker;
