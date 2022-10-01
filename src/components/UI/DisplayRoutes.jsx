c90459c1e7d8e3bb0191111d92ffa5cc29aea9bc
Nikolai Kocev <nikolai_kocev@hotmail.com>
2022-10-01 12:48:27 +0200

    changed ticket bg color to grey (figma)

--- src/components/UI/DisplayRoutes.jsx
+++ src/components/UI/DisplayRoutes.jsx
@@ -6,16 +6,16 @@ export default function DisplayRoutes({ props }) {
   let { startStation, endStation, arrivalTimeTo, departureTimeFrom } = props;
 
   return (
-    <div className="wrapper">
-      <div className="route-card">
-        <Container fluid className="bg-primary text-white mt-2 ">
-          <Row className="py-3">
-            <Col className="routeSlot">
+    <div className='wrapper'>
+      <div className='route-card'>
+        <Container fluid className='bg-secondary text-white mt-2 '>
+          <Row className='py-3'>
+            <Col className='routeSlot'>
               <p>{`${startStation} - ${endStation}`}</p>
             </Col>
           </Row>
-          <Row className="py-3">
-            <Col className="timeSlot">
+          <Row className='py-3'>
+            <Col className='timeSlot'>
               <p>{`${arrivalTimeTo} - ${departureTimeFrom}`}</p>
             </Col>
           </Row>
