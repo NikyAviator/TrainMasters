ebcc4c241bf6c4e7221c3f95da028a2582f833ca
Nikolai Kocev <nikolai_kocev@hotmail.com>
2022-10-04 15:11:51 +0200

    Mayyadah, Sean, Argjend & Nikolai, fixing Search Button in correct place

--- src/components/Pages/BookingPage.jsx
+++ src/components/Pages/BookingPage.jsx
@@ -1,12 +1,21 @@
 import '../../../scss/main.scss';
 import TicketFromTo from '../Ticket/TicketFromTo';
 import TicketTravelers from '../Ticket/TicketTravelers';
+import { Button } from '../UI/Button';
 
-export default function BookingPage() {
+export default function BookingPage(props) {
   return (
     <div>
       <TicketFromTo />
       <TicketTravelers />
+      <Button
+        className='bookingSearch'
+        buttonStyle='btn--secondary-outline'
+        buttonSize='btn--medium-secondary'
+        onClick={props.submitForm}
+      >
+        Sök
+      </Button>
     </div>
   );
 }
