10c23ac6894fec11d7e9653cce6b411887d73d78
Nikolai Kocev <nikolai_kocev@hotmail.com>
2022-10-05 14:36:33 +0200

    small fix to get main branch working

--- src/components/UI/Train.jsx
+++ src/components/UI/Train.jsx
@@ -1,21 +1,21 @@
-import { useEffect, useState } from "react";
-import TicketsPage from "../Pages/TicketsPage";
-import Carriage from "./Carriage";
+import { useEffect, useState } from 'react';
+import TicketsPage from '../Pages/TicketsPage';
+import Carriage from './Carriage';
 
 export default function Train({ props }) {
   let { trainId } = props;
-  const [image, setImage] = useState("");
+  const [image, setImage] = useState('');
   const [carriage, setCarriage] = useState(0);
   useEffect(() => {
     switch (trainId) {
       case 1:
-        setImage("../../../public/images/tåg1.jpg");
+        setImage('../../../public/images/tåg1.jpg');
         break;
       case 2:
-        setImage("../../../public/images/tåg1.jpg");
+        setImage('../../../public/images/tåg1.jpg');
         break;
       case 3:
-        setImage("../../../public/images/tåg1.jpg");
+        setImage('../../../public/images/tåg1.jpg');
         break;
     }
   });
@@ -29,10 +29,10 @@ export default function Train({ props }) {
           className='train'
           style={{
             backgroundImage: `url(${image})`,
-            width: "80%",
-            height: "130px",
-            backgroundRepeat: "no-repeat",
-            position: "absolute",
+            width: '80%',
+            height: '130px',
+            backgroundRepeat: 'no-repeat',
+            position: 'absolute',
           }}
         >
           <span className='vagn1' onClick={() => setCarriage(1)}>
@@ -54,7 +54,7 @@ export default function Train({ props }) {
             vagn6
           </span>
         </div>
-      )}{" "}
+      )}{' '}
     </>
   );
 }
