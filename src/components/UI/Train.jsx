37a7f1c5ce80cd43952618f366ba2236d1e75dd7
Seans99 <sean.schelvis@hotmail.com>
2022-10-09 14:23:44 +0200

    Added code that might fix displaying of correct trains

--- src/components/UI/Train.jsx
+++ src/components/UI/Train.jsx
@@ -27,35 +27,121 @@ export default function Train({ props }) {
       {carriage ? (
         <Carriage carriage={carriage} props={props} />
       ) : (
-        <div className='wrapperTrain'>
-          <Container>
-            <Row
-              className='train'
-              style={{
-                backgroundImage: `url(${image})`,
-                minWidth: '100%',
-              }}
-            >
-              <div className='vagn1' onClick={() => setCarriage(1)}>
-                vagn1
-              </div>
-              <div className='vagn2' onClick={() => setCarriage(2)}>
-                vagn2
-              </div>
-              <div className='vagn3' onClick={() => setCarriage(3)}>
-                vagn3
-              </div>
-              <div className='vagn4' onClick={() => setCarriage(2)}>
-                vagn4
-              </div>
-              <div className='vagn5' onClick={() => setCarriage(2)}>
-                vagn5
-              </div>
-              <div className='vagn6' onClick={() => setCarriage(4)}>
-                vagn6
-              </div>
-            </Row>
-          </Container>
+          <div className="wrapperTrain">
+            <Container>
+              <Row
+                className="train"
+                style={{
+                  backgroundImage: `url(${image})`,
+                  minWidth: "100%",
+                }}
+              >
+                <div className="vagn1" onClick={() => setCarriage(1)}>
+                  vagn1
+                </div>
+                <div className="vagn2" onClick={() => setCarriage(2)}>
+                  vagn2
+                </div>
+                <div className="vagn3" onClick={() => setCarriage(3)}>
+                  vagn3
+                </div>
+                <div className="vagn4" onClick={() => setCarriage(2)}>
+                  vagn4
+                </div>
+                <div className="vagn5" onClick={() => setCarriage(2)}>
+                  vagn5
+                </div>
+                <div className="vagn6" onClick={() => setCarriage(4)}>
+                  vagn6
+                </div>
+              </Row>
+            </Container>
+
+            
+            {
+          /* 
+          MÅSTE TESTAS (MIN DATABAS FUNKAR INTE)
+          {trainId === 1 && (
+            <Container>
+              <Row
+                className="train"
+                style={{
+                  backgroundImage: `url(${image})`,
+                  minWidth: "100%",
+                }}
+              >
+                <div className="vagn1" onClick={() => setCarriage(1)}>
+                  vagn1
+                </div>
+                <div className="vagn2" onClick={() => setCarriage(2)}>
+                  vagn2
+                </div>
+                <div className="vagn3" onClick={() => setCarriage(3)}>
+                  vagn3
+                </div>
+                <div className="vagn4" onClick={() => setCarriage(2)}>
+                  vagn4
+                </div>
+                <div className="vagn5" onClick={() => setCarriage(2)}>
+                  vagn5
+                </div>
+                <div className="vagn6" onClick={() => setCarriage(4)}>
+                  vagn6
+                </div>
+              </Row>
+            </Container>
+          )}
+          {trainId === 2 && (
+            <Container>
+              <Row
+                className="train"
+                style={{
+                  backgroundImage: `url(${image})`,
+                  minWidth: "100%",
+                }}
+              >
+                <div className="vagn1" onClick={() => setCarriage(1)}>
+                  vagn1
+                </div>
+                <div className="vagn2" onClick={() => setCarriage(2)}>
+                  vagn2
+                </div>
+                <div className="vagn3" onClick={() => setCarriage(3)}>
+                  vagn3
+                </div>
+                <div className="vagn4" onClick={() => setCarriage(2)}>
+                  vagn4
+                </div>
+                <div className="vagn5" onClick={() => setCarriage(2)}>
+                  vagn5
+                </div>
+              </Row>
+            </Container>
+          )}
+          {trainId === 3 && (
+            <Container>
+              <Row
+                className="train"
+                style={{
+                  backgroundImage: `url(${image})`,
+                  minWidth: "100%",
+                }}
+              >
+                <div className="vagn1" onClick={() => setCarriage(1)}>
+                  vagn1
+                </div>
+                <div className="vagn2" onClick={() => setCarriage(2)}>
+                  vagn2
+                </div>
+                <div className="vagn3" onClick={() => setCarriage(3)}>
+                  vagn3
+                </div>
+                <div className="vagn4" onClick={() => setCarriage(2)}>
+                  vagn4
+                </div>
+              </Row>
+            </Container>
+              )} */}
         </div>
       )}
     </>
