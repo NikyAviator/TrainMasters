c086817bc3578e0655ab1d5555246b9af3660b86
argjend551 <argjend551@hotmail.com>
2022-10-10 16:18:14 +0200

    added views

---
+++ _seedDB/tables/04-timeTables.sql
@@ -0,0 +1,12 @@
+CREATE TABLE timeTables  (
+  id int(11) unsigned NOT NULL AUTO_INCREMENT,
+  startTime TIME NOT NULL,
+  routeId int(11) NOT NULL,
+	trainId int(11) NOT NULL,
+  notWeekends BOOL NOT NULL,
+  PRIMARY KEY (id),
+  KEY routeId (routeId),
+  KEY trainId (trainId),
+  CONSTRAINT _timeTables_routeId FOREIGN KEY (Id) REFERENCES routes (Id),
+  CONSTRAINT _timeTables_trainId FOREIGN KEY (Id) REFERENCES trains (Id)
+);
---
+++ _seedDB/tables/08-trainCarriages.sql
@@ -0,0 +1,12 @@
+CREATE TABLE trainCarriages  (
+  id int(11) unsigned NOT NULL AUTO_INCREMENT,
+  trainId int(11) NOT NULL,
+  carriage int(11) NOT NULL,
+  carriagesTypeId int(11) NOT NULL,
+  PRIMARY KEY (id),
+  KEY trainId (trainId),
+  KEY carriagesTypeId (Id),
+  CONSTRAINT _trainCarriages_trainId FOREIGN KEY (Id) REFERENCES trains (Id),
+  CONSTRAINT _trainCarriages_carriagesTypeId FOREIGN KEY (Id) REFERENCES carriagesTypes (Id)
+);
+
