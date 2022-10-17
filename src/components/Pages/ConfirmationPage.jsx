3a714b5592cf650a86f231f3dab5473bb5e15cb7
Seans99 <sean.schelvis@hotmail.com>
2022-10-17 10:43:51 +0200

    Added qr code package and added new page

---
+++ src/components/Pages/ConfirmationPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import QRCode from "react-qr-code";
+
+export default function ConfirmationPage() {
+  return (
+    <div>
+      <QRCode />
+    </div>
+  );
+}
