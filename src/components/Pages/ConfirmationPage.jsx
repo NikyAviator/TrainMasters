d7c7074c79c245fd25b4f746e275672f93451a62
Seans99 <sean.schelvis@hotmail.com>
2022-10-17 18:26:50 +0200

    Added som styling for confirmationPage

--- src/components/Pages/ConfirmationPage.jsx
+++ src/components/Pages/ConfirmationPage.jsx
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
+import Card from "react-bootstrap/Card";
 
 export default function ConfirmationPage() {
   const location = useLocation();
@@ -26,31 +27,33 @@ export default function ConfirmationPage() {
 
   return (
     <>
-      <Container>
-        <Col>
-          <Row>
-            <h1>Bookningsbekräftelse</h1>
-          </Row>
-          <Row
-            style={{
-              height: 'auto',
-              margin: '0 auto',
-              maxWidth: 64,
-              width: '100%',
-            }}
-          >
-            <QRCode size={256} value={bookingId} viewBox={`0 0 256 256`} />
-          </Row>
-          <Row>
-            <h2>{`Bokningsnr: ${bookingId}`}</h2>
-          </Row>
-          <Row>
-            <p>{`${fromDeparture} - ${toDestination}`}</p>
-          </Row>
-          <Row>
-            <p>{`Total pris: ${price}kr`}</p>
-          </Row>
-        </Col>
+      <Container style={{ marginTop: "20%", maxWidth: "70%" }}>
+        <Card style={{ border: "none", textAlign: "center", padding: "5%" }}>
+          <Col>
+            <Row>
+              <h1>Bookningsbekräftelse</h1>
+            </Row>
+            <Row
+              style={{
+                height: "auto",
+                margin: "0 auto",
+                maxWidth: 64,
+                width: "100%",
+              }}
+            >
+              <QRCode size={256} value={bookingId} viewBox={`0 0 256 256`} />
+            </Row>
+            <Row>
+              <h2>{`Bokningsnr: ${bookingId}`}</h2>
+            </Row>
+            <Row>
+              <p>{`${fromDeparture} - ${toDestination}`}</p>
+            </Row>
+            <Row>
+              <p>{`Total pris: ${price}kr`}</p>
+            </Row>
+          </Col>
+        </Card>
       </Container>
     </>
   );
