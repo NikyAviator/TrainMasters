215961d5867b2998858c3bb63bf1b2de80d248c8
Nikolai Kocev <nikolai_kocev@hotmail.com>
2022-10-19 19:17:41 +0200

    Sean & Nikolai: added dep/arr time at bokningsbekräftelse

--- src/components/Pages/ConfirmationPage.jsx
+++ src/components/Pages/ConfirmationPage.jsx
@@ -1,16 +1,16 @@
-import React from "react";
-import QRCode from "react-qr-code";
-import { Link, useLocation } from "react-router-dom";
-import { useState, useEffect } from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
+import React from 'react';
+import QRCode from 'react-qr-code';
+import { Link, useLocation } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 
 export default function ConfirmationPage() {
   const location = useLocation();
   let props = location.state;
-  let [seats, setSeats] = useState("");
+  let [seats, setSeats] = useState('');
 
   let { bookingObj, countTravelers, selected } = props;
   let {
@@ -38,24 +38,24 @@ export default function ConfirmationPage() {
   return (
     <>
       <Container
-        style={{ marginTop: "20%", marginBottom: "20%", maxWidth: "70%" }}
+        style={{ marginTop: '20%', marginBottom: '20%', maxWidth: '70%' }}
       >
-        <Card style={{ border: "none", textAlign: "center", padding: "5%" }}>
+        <Card style={{ border: 'none', textAlign: 'center', padding: '5%' }}>
           <Col>
             <Row>
               <h1>Bokningsbekräftelse</h1>
             </Row>
             <Row
               style={{
-                height: "auto",
-                margin: "0 auto",
+                height: 'auto',
+                margin: '0 auto',
                 maxWidth: 200,
-                width: "100%",
+                width: '100%',
               }}
             >
               <QRCode size={256} value={bookingId} viewBox={`0 0 256 256`} />
             </Row>
-            <Row style={{ paddingBottom: "5%" }}>
+            <Row style={{ paddingBottom: '5%' }}>
               <h2>{`Bokningsnr: ${bookingId}`}</h2>
             </Row>
             <Row>
@@ -65,18 +65,24 @@ export default function ConfirmationPage() {
               <p>{`Total pris: ${price}kr`}</p>
             </Row>
             <Row>
-              <p>{"Platform: " + platform}</p>
+              <p>{'Platform: ' + platform}</p>
+            </Row>
+            <Row>
+              <p>{'Avgångs tid: ' + departure}</p>
+            </Row>
+            <Row>
+              <p>{'Ankomst tid: ' + arrival}</p>
             </Row>
             <Row>
               <p
-                style={{ textDecoration: "underline" }}
+                style={{ textDecoration: 'underline' }}
               >{`Bokade resenärer:`}</p>
               {Object.entries(countTravelers).map(([key, value], i) => (
-                <div>{value ? `${value}x ${key}` : ""}</div>
+                <div>{value ? `${value}x ${key}` : ''}</div>
               ))}
             </Row>
-            <Row style={{ paddingTop: "4%" }}>
-              <p style={{ textDecoration: "underline" }}>{`Bokade säten:`}</p>
+            <Row style={{ paddingTop: '4%' }}>
+              <p style={{ textDecoration: 'underline' }}>{`Bokade säten:`}</p>
               <p>{seats}</p>
             </Row>
           </Col>
