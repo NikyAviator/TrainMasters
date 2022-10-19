ac49eba6f1ea90915de0a972e57524d47514a9db
Nikolai Kocev <nikolai_kocev@hotmail.com>
2022-10-19 19:23:32 +0200

    Sean & Nikolai: Added arr/dep tider för Ticketpage

--- src/components/Pages/TicketsPage.jsx
+++ src/components/Pages/TicketsPage.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import "../../../scss/main.scss";
-import { factory } from "../../utilities/FetchHelper";
+import React from 'react';
+import '../../../scss/main.scss';
+import { factory } from '../../utilities/FetchHelper';
 const { booking } = factory;
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import useStates from "../../utilities/useStates";
-import { useState, useEffect } from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Card from "react-bootstrap/Card";
-import QRCode from "react-qr-code";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import useStates from '../../utilities/useStates';
+import { useState, useEffect } from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import QRCode from 'react-qr-code';
 
 export default function TicketsPage() {
   const [ticket, setTicket] = useState();
   let [ticketSeatsInfo, setticketSeatsInfo] = useState({});
   let [countTravelers, setcountTravelers] = useState({});
-  let [seats, setSeats] = useState("");
+  let [seats, setSeats] = useState('');
   let emptyFormValues = {
-    findBookingId: "",
+    findBookingId: '',
   };
   const [formValues, updateStateFormValue] = useStates({
     ...emptyFormValues,
@@ -50,11 +50,11 @@ export default function TicketsPage() {
     let student = 0;
     let youth = 0;
     for (const seat in ticketSeatsInfo) {
-      if (ticketSeatsInfo[seat] === "Vuxen") adults++;
-      if (ticketSeatsInfo[seat] === "Barn") child++;
-      if (ticketSeatsInfo[seat] === "Pensionär") senior++;
-      if (ticketSeatsInfo[seat] === "Student") student++;
-      if (ticketSeatsInfo[seat] === "Ungdom") youth++;
+      if (ticketSeatsInfo[seat] === 'Vuxen') adults++;
+      if (ticketSeatsInfo[seat] === 'Barn') child++;
+      if (ticketSeatsInfo[seat] === 'Pensionär') senior++;
+      if (ticketSeatsInfo[seat] === 'Student') student++;
+      if (ticketSeatsInfo[seat] === 'Ungdom') youth++;
     }
     let count = {
       Vuxen: adults,
@@ -73,15 +73,15 @@ export default function TicketsPage() {
   let { findBookingId } = formValues;
 
   return (
-    <div className="bookingForm">
-      <Form style={{ paddingBottom: "10%", paddingTop: "10%" }}>
+    <div className='bookingForm'>
+      <Form style={{ paddingBottom: '10%', paddingTop: '10%' }}>
         <Form.Group>
           <Form.Control
-            type="text"
-            name="findBookingId"
-            placeholder="Bokningsnummer"
+            type='text'
+            name='findBookingId'
+            placeholder='Bokningsnummer'
             required
-            maxLength="100"
+            maxLength='100'
             value={findBookingId}
             onChange={onChangeFormValue}
           />
@@ -89,8 +89,8 @@ export default function TicketsPage() {
       </Form>
 
       <Button
-        type="submit"
-        className="book-search-btn mb-6"
+        type='submit'
+        className='book-search-btn mb-6'
         onClick={submitForm}
         disabled={!findBookingId}
       >
@@ -101,26 +101,26 @@ export default function TicketsPage() {
         {ticket ? (
           <Container>
             <Card
-              style={{ border: "none", textAlign: "center", padding: "5%" }}
+              style={{ border: 'none', textAlign: 'center', padding: '5%' }}
             >
               <Col>
                 <Row
                   style={{
-                    height: "auto",
-                    margin: "0 auto",
+                    height: 'auto',
+                    margin: '0 auto',
                     maxWidth: 200,
-                    width: "100%",
-                    paddingBottom: "3%",
+                    width: '100%',
+                    paddingBottom: '3%',
                   }}
                 >
                   <QRCode
                     size={256}
-                    style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+                    style={{ height: 'auto', maxWidth: '100%', width: '100%' }}
                     value={ticket.bookingId}
                     viewBox={`0 0 256 256`}
                   />
                 </Row>
-                <Row style={{ paddingBottom: "5%" }}>
+                <Row style={{ paddingBottom: '5%' }}>
                   <h2>{`Bokningsnr: ${ticket.bookingId}`}</h2>
                 </Row>
                 <Row>
@@ -133,16 +133,22 @@ export default function TicketsPage() {
                   <p>{`Platform: ${ticket.platform}`}</p>
                 </Row>
                 <Row>
+                  <p>{`Avgångstid: ${ticket.departure}`}</p>
+                </Row>
+                <Row>
+                  <p>{`Ankomsttid: ${ticket.arrival}`}</p>
+                </Row>
+                <Row>
                   <p
-                    style={{ textDecoration: "underline" }}
+                    style={{ textDecoration: 'underline' }}
                   >{`Bokade resenärer:`}</p>
                   {Object.entries(countTravelers).map(([key, value], i) => (
-                    <div>{value ? `${value}x ${key}` : ""}</div>
+                    <div>{value ? `${value}x ${key}` : ''}</div>
                   ))}
                 </Row>
-                <Row style={{ paddingTop: "4%" }}>
+                <Row style={{ paddingTop: '4%' }}>
                   <p
-                    style={{ textDecoration: "underline" }}
+                    style={{ textDecoration: 'underline' }}
                   >{`Bokade säten:`}</p>
                   <p>{seats}</p>
                 </Row>
